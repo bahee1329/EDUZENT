7efa0ba243f0444429fae59b15d2422cdec2b90d
bahee1329 <baheera1999@gmail.com>
2025-03-12 13:34:48 +0530

    feautre:f-e-t:make change sin the nav bar logo

--- frontend-e-retro/app/presentation/components/Navbar/Navbar.tsx
+++ frontend-e-retro/app/presentation/components/Navbar/Navbar.tsx
@@ -60,12 +60,12 @@ const Navbar = () => {
                             <div className="flex flex-shrink-0 items-center">
                                 <img
                                     className="block h-12 w-40 lg:hidden"
-                                    src={'/assets/logo/logo.svg'}
-                                    alt="dsign-logo"
+                                    src={'/assets/logo/navbar_logo.png'}
+                                    alt="dsigner-logo"
                                 />
                                 <img
-                                    className="hidden h-full w-full lg:block"
-                                    src={'/assets/logo/logo.svg'}
+                                    className="hidden h-16 w-48 lg:block"
+                                    src={'/assets/logo/navbar_logo.png'}
                                     alt="dsign-logo"
                                 />
                             </div>
