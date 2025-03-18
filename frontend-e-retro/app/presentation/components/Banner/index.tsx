bbd5e6f70647ebd4c23f69dc82eb1949dcfbc61a
bahee1329 <baheera1999@gmail.com>
2025-03-18 18:07:18 +0530

    feature:fet:add chnages in index

--- frontend-e-retro/app/presentation/components/Banner/index.tsx
+++ frontend-e-retro/app/presentation/components/Banner/index.tsx
@@ -6,15 +6,15 @@ const Banner = () => {
         <div id="home-section" className='bg-lightkblue'>
             <div className="mx-auto max-w-7xl pt-20 sm:pb-24 px-6">
 
-                <div className='grid grid-cols-1 lg:grid-cols-12 space-x-1'>
+                <div className='grid grid-cols-1 lg:grid-cols-12 space-x-8'>
 
                     <div className='col-span-6 flex flex-col justify-evenly'>
                         <div className='flex gap-2 mx-auto lg:mx-0'>
                             <Image src="/assets/banner/check.svg" alt="check-image" width={20} height={20} />
-                            <h3 className='text-kellygreen text-sm font-semibold text-center lg:text-start'>Get 30% off on first enroll</h3>
+                            <h3 className='text-kellygreen text-sm font-semibold text-center lg:text-start'>Get 30% off on first 100</h3>
                         </div>
-                        <h1 className='text-midnightblue text-4xl sm:text-5xl font-semibold text-center lg:text-start lh-120 pt-5 lg:pt-0'>Advance your engineering skills with us.</h1>
-                        <h3 className='text-charcoal text-lg font-normal text-center lg:text-start opacity-75 pt-5 lg:pt-0'>Build skills with our courses and mentor from world-class companies.</h3>
+                        <h1 className='text-midnightblue text-4xl sm:text-5xl font-semibold text-center lg:text-start lh-120 pt-5 lg:pt-0'>Achieve great results with easy guidance at Advanced Level Education Center..</h1>
+                        <h3 className='text-charcoal text-lg font-normal text-center lg:text-start opacity-75 pt-5 lg:pt-0'>Build expertise through our courses and mentorship from world-class professionals.</h3>
 
                         <div className="relative text-white focus-within:text-white flex flex-row-reverse input-shadow rounded-full pt-5 lg:pt-0">
                             <input type="Email address" name="q" className="py-6 lg:py-8 text-lg w-full text-black opacity-75 rounded-full pl-8 focus:outline-none focus:text-black" placeholder="search courses..." autoComplete="off" />
@@ -32,17 +32,17 @@ const Banner = () => {
                             </div>
                             <div className='flex gap-2'>
                                 <Image src="/assets/banner/check-circle.svg" alt="check-image" width={30} height={30} className='smallImage'/>
-                                <p className='text-sm sm:text-lg font-normal text-black'>Learning path</p>
+                                <p className='text-sm sm:text-lg font-normal text-black'>Learning</p>
                             </div>
                             <div className='flex gap-2'>
                                 <Image src="/assets/banner/check-circle.svg" alt="check-image" width={30} height={30} className='smallImage'/>
-                                <p className='text-sm sm:text-lg font-normal text-black'>Community</p>
+                                <p className='text-sm sm:text-lg font-normal text-black'>Result</p>
                             </div>
                         </div>
                     </div>
 
                     <div className='col-span-6 flex justify-center'>
-                        <Image src="/assets/banner/mahila.png" alt="nothing" width={1000} height={805} />
+                        <Image src="/assets/banner/banner_img.png" alt="nothing" width={1000} height={805} />
                     </div>
                 </div>
             </div>
